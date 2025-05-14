7716173d7d1f0f6800b7cc4127258361ff580e61
NutDealer1000101 <lukand23@hotmail.no>
2025-05-14 21:45:57 +0200

    Some adjustments to file structure.

---
+++ ratatoskr_fc_notta/include/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Config settings that are used across the entire project
+
+// For example, we could define if we're compiling for debugging or release
+#define BUILD_DEBUG
---
+++ ratatoskr_fc_notta/include/pins.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Definitons for GPIO-pins and other peripherals to be used in code
+
+namespace pins {
+	// !!! Only example values and pins !!!
+	constexpr int LED = 3;
+	constexpr int RADIO = 12;
+}
