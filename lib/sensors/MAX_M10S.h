d8a3d50e29274f10929c7b1bd7a4a86b95ff83d4
Chris <chrismostro05@gmail.com>
2025-08-31 13:08:33 +0200

    Update MAX_M10S.h

--- lib/sensors/MAX_M10S.h
+++ lib/sensors/MAX_M10S.h
@@ -1,13 +1,26 @@
 #ifndef MAX_M10S_H
 #define MAX_M10S_H
 
-#include <stdint.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include "Sensor.h"
 
-bool maxM10SInit();
-void maxM10SUpdate();
-double maxM10SGetLatitude();
-double maxM10SGetLongitude();
-double maxM10SGetAltitude();
-uint8_t maxM10SGetSatellites();
+class MAXM10SSensor : public Sensor {
+public:
+    MAXM10SSensor(unsigned long interval = 1000); // default 1 Hz oppdatering
 
-#endif 
+    bool setup() override;
+    void update() override;
+    void info() override;
+
+    double getLatitude();
+    double getLongitude();
+    double getAltitude();
+    uint8_t getSatellites();
+
+private:
+    SFE_UBLOX_GNSS myGNSS;
+};
+
+#endif
