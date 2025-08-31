f55ffba048e76da8543942b7d048e712e70c03b6
Chris <chrismostro05@gmail.com>
2025-08-31 12:59:55 +0200

    Update kx134.h

--- lib/sensors/kx134.h
+++ lib/sensors/kx134.h
@@ -2,9 +2,21 @@
 #define KX134_H
 
 #include <Arduino.h>
+#include <SPI.h>
+#include "Sensor.h"  
 
-bool kx134Init();
-void kx134ReadRaw(int16_t &x, int16_t &y, int16_t &z);
-void kx134PrintXYZ();
+class KX134Sensor : public Sensor {
+public:
+    KX134Sensor(unsigned long interval = 100);
+
+    bool setup() override;
+    void update() override;
+    void info() override;
+
+private:
+    uint8_t readRegister(uint8_t reg);
+    void writeRegister(uint8_t reg, uint8_t val);
+    void readRaw(int16_t &x, int16_t &y, int16_t &z);
+};
 
 #endif
