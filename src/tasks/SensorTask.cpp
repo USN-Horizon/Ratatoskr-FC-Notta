2ab0ac8bb5034678a97575c4116f57291bb8dad1
Chris <chrismostro05@gmail.com>
2025-09-15 16:27:22 +0200

    Update SensorTask.cpp

--- src/tasks/SensorTask.cpp
+++ src/tasks/SensorTask.cpp
@@ -7,21 +7,29 @@
 TaskHandle_t sensorTaskHandle = NULL;
 
 KX134Sensor kx134(500);
+MAXM10SSensor gps(1000);
+
+
+void setupSensor(Sensor &sensor, const char* name){
+    if(!sensor.setup()){
+        Serial.print(F("[SensorTask] "));
+        Serial.print(name);
+        Serial.println(F(" init failed!"));
+    }
+    else {sensor.info();}    
+}
+
 
 void task_Sensor(void *pvParameters) {
     TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xFrequency = pdMS_TO_TICKS(SENSOR_TASK_PERIOD_MS);
 
-    if(!kx134.setup()){
-        Serial.println(F("[SensorTask] KX134 init failed!"));
-    }
-    else{
-        kx134.info();
-    }
-
+    setupSensor(kx134, "KX134");
+    setupSensor(gps, "MAX-M10S");
     
     while (1) {
         kx134.update();
+        gps.update();
         
         // Read IMU data
         
