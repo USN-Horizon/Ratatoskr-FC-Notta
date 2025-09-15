a80dc7b7e137da2cf76dee1c47f63c70509c2bbe
Chris <chrismostro05@gmail.com>
2025-09-15 17:39:57 +0200

    Create MS5611.h

---
+++ lib/sensors/MS5611.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Arduino.h>
+#include <Wire.h>
+#include "Sensor.h"
+
+// MS5611 I2C-adresse 
+#define MS5611_ADDR 0x77
+
+class MS5611 : public Sensor {
+public:
+    MS5611(String n = "MS5611");
+
+    bool setup() override;
+    void update() override;
+    void info() override;
+
+    float getPressure() const { return pressure; }  // i hPa
+    float getTemperature() const { return temperature; } // i °C
+
+private:
+    uint16_t C[7];      // Kalibreringsdata fra PROM
+    float pressure;
+    float temperature;
+
+    bool readPROM();
+    bool readADC(uint32_t &result, uint8_t cmd);
+    bool startConversion(uint8_t cmd);
+};
