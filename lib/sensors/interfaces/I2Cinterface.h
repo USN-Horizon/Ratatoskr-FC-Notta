7d9e13b629b0d3350d6b289b0319d8f814b87ea4
Chris <chrismostro05@gmail.com>
2025-10-05 13:03:47 +0200

    Update I2Cinterface.h

--- lib/sensors/interfaces/I2Cinterface.h
+++ lib/sensors/interfaces/I2Cinterface.h
@@ -1,1 +1,6 @@
-
+class I2CInterface {
+public:
+    virtual bool write(uint8_t addr, const uint8_t* data, size_t len) = 0;
+    virtual bool read(uint8_t addr, uint8_t* data, size_t len) = 0;
+    virtual ~I2CInterface() = default;
+};
