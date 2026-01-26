39d96ba36f48b0f1ff47bca54677f739bdf6dbb7
Hubert Rwechungura <hubert.rwechungura@gmail.com>
2026-01-27 00:55:59 +0100

    Implemented servo functionality with arbitrary angles and pin value

--- src/tasks/ActuationTask.cpp
+++ src/tasks/ActuationTask.cpp
@@ -1,36 +1,43 @@
 #include "ActuationTask.h"
 #include <cstddef>
+#include <iostream>
 
 TaskHandle_t actuationTaskHandle = NULL;
-
-
 
 
 void task_Actuation(void *pvParameters) {
     TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xFrequency = pdMS_TO_TICKS(ACTUATION_TASK_PERIOD_MS);
+    Servo.attach(Servo_Pin, 0, Servo_Angle);
     
     while (1) {
-        // Check flight state and control requirements
-        if (fc_data.flightstate == FlightState::NOT_READY) {
-			//not ready yet....
 
-		}
+        // Check flight state and control requirements
+        while (fc_data.flightstate == FlightState::NOT_READY || fc_data.flightstate == FlightState::ARMED)
+        {
+            if (Servo.attached() == false)
+            {
+                std::cerr << "Servo attachment has failed" << std::endl;
+            }
+        }
 
         // Calculate servo/motor commands
+        constexpr auto min_angle = Servo_Angle / 2;
 
         // Execute deployment sequences (drougue, and main)
         switch (fc_data.flightstate) {
         case FlightState::DROUGE:
             // first deployment sequence:
-
+			Servo.write(min_angle);
 			break;
         case FlightState::MAIN:
             // second deployment sequence:
-
+			Servo.write(Servo_Angle);
 			break;
 		}
+
         // Safety checks and failsafe operations
+
         
         vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
--- src/tasks/ActuationTask.h
+++ src/tasks/ActuationTask.h
@@ -4,16 +4,21 @@
 #define ACTUATION_TASK_STACK_SIZE 256
 #define ACTUATION_TASK_PRIORITY 5
 #define ACTUATION_TASK_PERIOD_MS 50
+#define Servo_Angle 180 //angle variable for reference, will be changed once we get more information
+#define Servo_Pin 10  // pin number is unkown as of right now, given an arbitrary value
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
 #include "../fc_data.h"
+#include <Servo.h>
 
 extern FC_Data fc_data;
 extern SemaphoreHandle_t fc_data_mutex;
 
 extern TaskHandle_t actuationTaskHandle;
 
+extern Servo Servo;
 
 void task_Actuation(void *pvParameters);
 void init_task_Actuation(void);
